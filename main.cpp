9af8c3947a0affd15fefb6174415a46f38e8317c
Jacob Fischer <jacob.t.fischer@gmail.com>
2015-08-09 22:02:08 -0500

    argparse like options standard as with all clients

--- main.cpp
+++ main.cpp
@@ -1,19 +1,49 @@
 #include <iostream>
 #include <string>
-#include <boost/asio.hpp>
+#include <boost/program_options.hpp>
 #include "joueur/client.h"
 #include "joueur/baseGame.h"
 #include "joueur/baseGameManager.h"
 #include "currentGame.h"
 
-int main()
+int main(int argc, char* argv[])
 {
-    std::string gameName = "Checkers";
-    std::string server = "localhost";
-    std::string port = "3000";
-    std::string playerName;
-    std::string requestedSession = "*";
-    bool printIO = true;
+    namespace po = boost::program_options;
+    po::options_description desc("Runs the C++ client with options. Must a provide a game name to play on the server.");
+    desc.add_options()
+        ("help", "produce help message")
+        ("game", po::value<std::string>(), "the name of the game you want to play on the server")
+        ("server,s", po::value<std::string>()->default_value("localhost"), "the url to the server you want to connect to e.g. locahost:3000")
+        ("port,p", po::value<std::string>()->default_value("3000"), "the port to connect to on the server. Can be defined on the server arg via server:port")
+        ("playerName,n", po::value<std::string>()->default_value(""), "the name you want to use as your AI\'s player name. This over-rides the name you set in your code")
+        ("session,r", po::value<std::string>()->default_value("*"), "the requested game session you want to play on the server")
+        ("printIO", "(debugging) print IO through the TCP socket to the terminal");
+
+    po::positional_options_description p;
+    p.add("game", 1);
+    po::variables_map vm;
+    po::store(po::command_line_parser(argc, argv).options(desc).positional(p).run(), vm);
+    po::notify(vm);
+
+    bool help = (vm.count("help") > 0);
+    if (!vm.count("game") && !help)
+    {
+        std::cerr << "Error: game name not supplied. Inlcude the name of the game you want as the first positional argument.\n";
+        help = true;
+    }
+
+    if (help)
+    {
+        std::cout << desc << "\n";
+        return 1;
+    }
+
+    std::string gameName = vm["game"].as<std::string>();
+    std::string server = vm["server"].as<std::string>();
+    std::string port = vm["port"].as<std::string>();
+    std::string playerName = vm["playerName"].as<std::string>();
+    std::string requestedSession = vm["session"].as<std::string>();
+    bool printIO = (vm.count("printIO") > 0);
 
     Joueur::Client *client = Joueur::Client::getInstance();
 
