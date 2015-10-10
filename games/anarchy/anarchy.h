67340bef35c5dd4c9a52aebe504de380e3c9ceed
Jacob Fischer <jacob.t.fischer@gmail.com>
2015-10-09 22:44:47 -0500

    Added Anarchy codegen'd files

---
+++ games/anarchy/anarchy.h
@@ -0,0 +1,20 @@
+#ifndef JOUEUR_ANARCHY_H
+#define JOUEUR_ANARCHY_H
+
+namespace Anarchy
+{
+    class Building;
+    class FireDepartment;
+    class Forecast;
+    class GameObject;
+    class Player;
+    class PoliceDepartment;
+    class Warehouse;
+    class WeatherStation;
+
+    class Game;
+    class GameManager;
+    class AI;
+}
+
+#endif
