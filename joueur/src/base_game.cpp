a1d1c971d1416a791c643ad4637b6b1de158d6fe
Adam Harter <athb79@mst.edu>
2016-04-04 12:32:28 -0500

    fixed vis

--- joueur/src/base_game.cpp
+++ joueur/src/base_game.cpp
@@ -121,7 +121,7 @@ Any Base_game::handle_response(const std::string& expected)
       ai_->print_win_loss_info();
       //output URL info
       const auto& data = attr_wrapper::get_loc(doc, "data")->value;
-      const auto url = attr_wrapper::get_loc(doc, "visualizerURL");
+      const auto url = data.FindMember(doc, "visualizerURL");
       if(url != data.MemberEnd())
       {
          const auto url2 = attr_wrapper::as<std::string>(url->value);
