fe59b47902816cb82aa412a33ed82103d9a20a48
Matthew Nuckolls <matthew.nuckolls@gmail.com>
2016-04-07 09:39:14 -0400

    Remove unneeded files

--- _creer/games/${lowercase_first(game_name)}/registry.h
+++
@@ -1,9 +0,0 @@
-#ifndef JOUEUR_${game_name.upper()}_REGISTERY_H
-#define JOUEUR_${game_name.upper()}_REGISTERY_H
-
-#include "../../joueur/baseGameManager.h"
-#include "gameManager.h"
-
-REGISTER_FUNCTION(gamesRegistry, ${game_name}, []() -> Joueur::BaseGameManager* { return new ${game_name}::GameManager(); })
-
-#endif
--- games/anarchy/registry.h
+++
@@ -1,9 +0,0 @@
-#ifndef JOUEUR_ANARCHY_REGISTERY_H
-#define JOUEUR_ANARCHY_REGISTERY_H
-
-#include "../../joueur/baseGameManager.h"
-#include "gameManager.h"
-
-REGISTER_FUNCTION(gamesRegistry, Anarchy, []() -> Joueur::BaseGameManager* { return new Anarchy::GameManager(); })
-
-#endif
